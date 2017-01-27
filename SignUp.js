300d8a34d1afade4a5f46701d068de79194bfe17
nikhil1028 <nikhil.raj.cse13@itbhu.ac.in>
2017-01-27 06:54:15 +0530

    Overlays and footer
    Footer added..
    SignUp Overlay and Explore overlay.

--- SignUp.js
+++ SignUp.js
@@ -1,5 +1,6 @@
 function openSignUpOverlay() {
     document.getElementById("SignUp").style.width = "100%";
+	document.getElementById("SignUpForms").style.opacity = "1";
 	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
 	document.getElementById("SignUpForms").style.display = "block";
 }
@@ -7,12 +8,32 @@ function openSignUpOverlay() {
 /* Close when someone clicks on the "x" symbol inside the explore */
 function closeSignUpOverlay() {
 	document.getElementById("SignUp").style.width = "0%";
+	document.getElementById("SignUpForms").style.opacity = "0";
+	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+}
+
+function openLogInOverlay() {
+	document.getElementById("LogIn").style.width = "100%";
+	document.getElementById("LogInForms").style.opacity = "1";
+	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+}
+
+/* Close when someone clicks on the "x" symbol inside the explore */
+function closeLogInOverlay() {
+	document.getElementById("LogIn").style.width = "0%";
+	document.getElementById("LogInForms").style.opacity = "0";
 	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.SignUpSpan')) {
+    if (event.target.matches('.translucentContainer')) {
         document.getElementById("SignUp").style.width = "0%";
+		document.getElementById("LogIn").style.width = "0%";
+		document.getElementById("SignUpForms").style.opacity = "0";
+		document.getElementById("LogInForms").style.opacity = "0";
+		document.getElementById("myNav").style.width = "0";
+		document.getElementById("exploreContent").style.width = "0";
+		document.getElementById("exploreContent").style.opacity = "0";
 		document.getElementsByTagName("body")[0].style.overflowY = "scroll";
     }
 }
@@ -29,33 +50,4 @@ function SignMeUp() {
 function logout() {
 	document.getElementById("registrationDiv").style.display = "inline-block";
 	document.getElementById("Profile").style.display = "none";
-}
-
-function openSignUpForms(evt, registrationType) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(registrationType).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-
-
-function openLogInOverlay() {
-	document.getElementById("LogIn").style.width = "100%";
-    document.getElementById("LogInOverlay").style.display = "block";
-	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
-}
-
-/* Close when someone clicks on the "x" symbol inside the explore */
-function closeLogInOverlay() {
-	document.getElementById("LogIn").style.width = "0%";
-	document.getElementById("LogInOverlay").style.display = "none";
-	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
--- explore.js
+++ explore.js
@@ -1,39 +1,35 @@
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+	document.getElementById("exploreContent").style.width = "25%";
 	document.getElementById("exploreContent").style.opacity = "1";
+	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
 }
 
 function closeNav() {
     document.getElementById("myNav").style.width = "0";
+	document.getElementById("exploreContent").style.width = "0";
 	document.getElementById("exploreContent").style.opacity = "0";
-}
-
-var modal = document.getElementById('myNav');
-
-window.onclick = function(event) {
-    if (event.target !== modal) {
-        modal.style.width = "0";
-    }
+	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
 
 function myfunction1(){
 	var div = document.getElementById('locationInput');
-    if (div.style.display !== 'block') {
-        div.style.display = 'block';
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
     }
     else {
-        div.style.display = 'none';
+        div.style.display = 'block';
     }
 }
 
 function myfunction2() {
 	var div = document.getElementById('fundingInput');
-    if (div.style.display !== 'block') {
-        div.style.display = 'block';
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
     }
     else {
-        div.style.display = 'none';
+        div.style.display = 'block';
     }
 }
 
