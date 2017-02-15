6556df8e5840b68f0119eea98ac9f246881aa927
nikhil1028 <nikhil.raj.cse13@itbhu.ac.in>
2017-02-15 21:02:44 +0530

    font changes and team page and payment methods

--- userPage.js
+++ userPage.js
@@ -14,6 +14,22 @@ function openUserPageLeftSideTabs(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+function openUserPagePaymentOptionsTabs(evt, tabName) {
+    var i, mainTabContent, mainTabLinks;
+    mainTabContent = document.getElementsByClassName("userPagePaymentOptionsLinksContent");
+    for (i = 0; i < mainTabContent.length; i++) {
+        mainTabContent[i].style.display = "none";
+		mainTabContent[i].style.width = "0";
+    }
+    mainTabLinks = document.getElementsByClassName("userPagePaymentOptionsLinks");
+    for (i = 0; i < mainTabLinks.length; i++) {
+        mainTabLinks[i].className = mainTabLinks[i].className.replace(" active1", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+	document.getElementById(tabName).style.width = "75%";
+    evt.currentTarget.className += " active1";
+}
+
 
 var $badges = $('.js-progress-badge')
   , $window = $(window)
